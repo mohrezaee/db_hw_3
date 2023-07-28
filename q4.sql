eb7e4294623b4c00c715c8f6c1b868932555a2a9
R.Akhlaghian <82227266+R-Akhlaghian@users.noreply.github.com>
2023-07-28 21:04:27 +0330

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -9,12 +9,12 @@ WHERE r.residence_id NOT IN
 FROM residence r
 INNER JOIN host ON r.host_id = host.national_code
 INNER JOIN rent ON rent.residence_id = r.residence_id
-INNER JOIN city ON city.city_id = r.city_id)
+INNER JOIN city ON city.city_id = r.city_id
+-- WHERE rent.rent_from < y AND rent.rent_to > x
+  )
 AND city.city_name = 'Tabriz' AND r.capacity >= 4
 
 -- AND r.long > x1 AND r.long < x2 AND r.lat > y1 AND r.lat < y2
-
--- AND rent.rent_from < y AND rent.rent_to > x
 
 ORDER BY (SELECT AVG(comment.rating) FROM residence res
 INNER JOIN comment ON res.residence_id = comment.residence_id
